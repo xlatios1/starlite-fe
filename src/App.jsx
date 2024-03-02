83e1cecf7cd2669aae1cc8f350a998ab5f02c18d
yijun <woonyijun10@gmail.com>
2024-03-02 12:52:49 +0800

    fixed ui bug

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ export default function App() {
 				{/* </article>
 			</div> */}
 			</Router>
-			<div className="version-tag">Beta version: 2.14.3</div>
+			<div className="version-tag">Beta version: 2.14.5</div>
 		</AuthContextProvider>
 	)
 }

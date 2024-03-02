ba6702d9b74ffe77ab21720654626677c12ad5f1
yijun <woonyijun10@gmail.com>
2024-03-02 16:50:14 +0800

    Major refactor

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,4 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Refdfvedfd
+export default function NotFound() {
+	return <>Not Found!!</>
+}
---
+++ src/pages/Registration/Registration.jsx
@@ -0,0 +1,147 @@
+import { useLayoutEffect, useState, useRef, useEffect } from 'react'
+import '@styles/signin.css'
+import Notification from '@components/notification/notification.tsx'
+import RenderAccountError from '@components/errorhandling/Errors.tsx'
+import FocusTextBox from '@components/errorhandling/Focus.tsx'
+import Loading from '@components/loading/Loading.tsx'
+import { UserAuth } from '@authentications/AuthContext.js'
+import { handleEmailChanges } from '@root/components/emailhandler/handleEmailChanges.tsx'
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function Registration() {
+	const registerEmail = useRef(null)
+	const registerPassword = useRef(null)
+	const registerRetypePassword = useRef(null)
+	const [registration, setRegistration] = useState({
+		newEmail: '@e.ntu.edu.sg',
+		newPassword: '',
+		retypePassword: '',
+	})
+	const [isLoading, setIsLoading] = useState(false)
+	const [errorMessages, setErrorMessages] = useState([])
+
+	const { createUser, fetchUserInCache } = UserAuth()
+	const navigate = useNavigate()
+
+	const curUser = fetchUserInCache()
+	useEffect(() => {
+		if (
+			curUser &&
+			curUser?.stsTokenManager?.expirationTime > new Date().getTime()
+		) {
+			console.log('Active user cache present, directing...', curUser)
+			navigate('/home')
+		}
+	}, [])
+
+	const handleCreateUser = async (e) => {
+		e.preventDefault()
+		let errMsg = []
+		if (registration.newEmail.length < 14) {
+			// Email not found
+			errMsg = [...errMsg, 'registerEmail']
+			FocusTextBox({ ref: registerEmail })
+		}
+		if (!registration.newPassword) {
+			errMsg = [...errMsg, 'registerPassword']
+			FocusTextBox({ ref: registerRetypePassword })
+		}
+		if (registration.newPassword !== registration.retypePassword) {
+			// Invalid password
+			errMsg = [...errMsg, 'differentPassword']
+			FocusTextBox({ ref: registerRetypePassword })
+		}
+		setErrorMessages(Array.from(new Set(errMsg)))
+		if (errMsg.length === 0) {
+			setIsLoading(true)
+			const isCreated = await createUser(
+				registration.newEmail,
+				registration.newPassword
+			)
+			if (isCreated === true) {
+				Notification('success', 'Account creation successful!', 2000)
+				navigate('/home')
+			} else {
+				Notification('error', isCreated.message.split(': ')[1], 3000)
+			}
+			setIsLoading(false)
+		}
+	}
+
+	const handleChange = (e) => {
+		let value = e.target.value
+		if (e.target.name === 'newEmail') {
+			value = handleEmailChanges(value)
+		}
+		setRegistration((prev) => ({
+			...prev,
+			[e.target.name]: value,
+		}))
+	}
+	//runs synchronously before the repaint, eliminating the blink.
+	useLayoutEffect(() => {
+		registerEmail.current.setSelectionRange(
+			registration.newEmail.length - 13,
+			registration.newEmail.length - 13
+		)
+	}, [registration.newEmail])
+
+	return (
+		<div className="login-initializer">
+			{isLoading && <Loading />}
+			<form className="form">
+				<h3>Sign up for a free account</h3>
+				<p>
+					Already have an account? <Link to="/">Sign in.</Link>
+				</p>
+				<div className="form-control">
+					<label htmlFor="Email"> Email: </label>
+					<input
+						type="text"
+						name="newEmail"
+						value={registration.newEmail}
+						ref={registerEmail}
+						onChange={handleChange}
+					/>
+				</div>
+				{RenderAccountError({
+					errorMessages: errorMessages,
+					name: 'registerEmail',
+				})}
+				<div className="form-control">
+					<label htmlFor="password"> Password: </label>
+					<input
+						type="password"
+						name="newPassword"
+						value={registration.newPassword}
+						ref={registerPassword}
+						onChange={handleChange}
+					/>
+				</div>
+				{RenderAccountError({
+					errorMessages: errorMessages,
+					name: 'registerPassword',
+				})}
+				<div className="form-control">
+					<label htmlFor="confirmPassword"> Confirm Password: </label>
+					<input
+						type="password"
+						name="retypePassword"
+						value={registration.retypePassword}
+						ref={registerRetypePassword}
+						onChange={handleChange}
+					/>
+				</div>
+				{RenderAccountError({
+					errorMessages: errorMessages,
+					name: 'differentPassword',
+				})}
+				<div>
+					<button className="btn" onClick={handleCreateUser}>
+						Create
+					</button>
+				</div>
+			</form>
+		</div>
+	)
+}

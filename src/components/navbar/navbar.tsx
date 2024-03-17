d17d3bb523df0a14e03a53660850e0ab96d984d7
Yi Jun <woonyijun@WOONs-MacBook-Air.local>
2024-03-17 15:18:30 +0800

    Clear state upon logout

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,6 +4,7 @@ import { Outlet, Link, useNavigate } from 'react-router-dom'
 import { UserAuth } from '@authentications/AuthContext.js'
 import Notification from '@components/notification/notification.tsx'
 import './navbar.css'
+import { useDispatch } from 'react-redux'
 
 type NavBarProp = {
 	user: string
@@ -14,11 +15,13 @@ export default function NavBar({ user, active }: NavBarProp) {
 	const [isMenuClick, setIsMenuClick] = useState(true)
 	const { logout } = UserAuth()
 	const navigate = useNavigate()
+	const dispatch = useDispatch()
 
 	const handleLogout = async () => {
 		const isLogout = await logout()
 		if (isLogout.status === 200) {
 			navigate('/')
+			dispatch({ type: 'LOGOUT' })
 			Notification('success', 'Logout successful!', 1000)
 		} else {
 			Notification('error', isLogout.message, 3000)
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {
+	PayloadAction,
+	combineReducers,
+	configureStore,
+} from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import loadingSlice from './loading/loadingSlice.ts'
 import { courseApi } from '@store/course/courseApi.ts'
@@ -6,20 +10,32 @@ import courseSlice from './course/courseSlice.ts'
 import walkthroughSlice from './walkthrough/walkthroughSlice.ts'
 import previewSlice from './preview/previewSlice.ts'
 import timetableSlice from './timetable/timetableSlice.ts'
-export type RootState = ReturnType<typeof store.getState>
+
+const appReducer = combineReducers({
+	loading: loadingSlice.reducer,
+	course: courseSlice.reducer,
+	preview: previewSlice.reducer,
+	timetable: timetableSlice.reducer,
+	walkthrough: walkthroughSlice.reducer,
+	[courseApi.reducerPath]: courseApi.reducer,
+})
+
+const rootReducer = (
+	state: ReturnType<typeof appReducer> | undefined,
+	action: PayloadAction<string | null>
+) => {
+	if (action.type === 'LOGOUT') {
+		return appReducer(undefined, action)
+	}
+	return appReducer(state, action)
+}
 
 export const store = configureStore({
-	reducer: {
-		loading: loadingSlice.reducer,
-		course: courseSlice.reducer,
-		preview: previewSlice.reducer,
-		timetable: timetableSlice.reducer,
-		walkthrough: walkthroughSlice.reducer,
-		[courseApi.reducerPath]: courseApi.reducer,
-	},
+	reducer: rootReducer,
 	middleware: (getDefaultMiddleware) => {
 		return getDefaultMiddleware().concat(courseApi.middleware)
 	},
 })
 
 setupListeners(store.dispatch)
+export type RootState = ReturnType<typeof store.getState>
